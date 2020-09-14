b1818290477a5380ad4291e6943dadc0c6925ac3
Tina Safari <tina.safari85@gmail.com>
2020-09-14 14:41:41 -0400

    added Action

---
+++ src/Actions/authUser.js
@@ -0,0 +1,16 @@
+export const SET_AUTH_USER = 'SET_AUTH_USER'
+// export const LOG_OUT = "LOG_OUT"
+
+export function setAuthUser(id) {
+    return {
+        type: SET_AUTH_USER,
+        id
+    }
+}
+
+// export function logOut(id) {
+//     return {
+//         type: LOG_OUT,
+//         id
+//     }
+// }
