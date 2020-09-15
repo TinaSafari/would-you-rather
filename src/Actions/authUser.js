420309c54eed510939f2e5b36ec7ce28e9339157
Tina Safari <tina.safari85@gmail.com>
2020-09-15 12:48:14 -0400

    fix action/reducer the authUser

--- src/Actions/authUser.js
+++ src/Actions/authUser.js
@@ -1,17 +1,17 @@
-// export const SET_AUTH_USER = 'SET_AUTH_USER'
-// export const LOG_OUT = "LOG_OUT"
+export const SET_AUTH_USER = 'SET_AUTH_USER'
+export const LOG_OUT = "LOG_OUT"
 
-// export function setAuthUser(id) {
-//     return {
-//         type: SET_AUTH_USER,
-//         id
-//     }
-// }
+export function setAuthUser(id) {
+    return {
+        type: SET_AUTH_USER,
+        id
+    }
+}
 
-// export function logOut(id) {
-//     return {
-//         type: LOG_OUT,
-//         id
-//     }
-// }
+export function logOut(id) {
+    return {
+        type: LOG_OUT,
+        id
+    }
+}
 
