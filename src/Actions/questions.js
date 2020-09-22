bde4ce500a6c64c4798416d5ca28fdbc63adf75d
Tina Safari <tina.safari85@gmail.com>
2020-09-22 16:16:54 -0400

    newQuestion UI added

--- src/Actions/questions.js
+++ src/Actions/questions.js
@@ -1,3 +1,6 @@
+import {saveQuestion, saveQuestionAnswer} from "../Utils/api"
+import {handleInitialData} from './shared'
+import {hideLoading, showLoading} from "react-redux-loading-bar";
 
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 
@@ -5,6 +8,32 @@ export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions
+    }
+}
+//something wrong here>>>>>>>>>
+export function handleAddQuestion(answerObj) {
+    return dispatch => {
+        dispatch(showLoading())
+
+        return saveQuestionAnswer({
+            ...answerObj
+        })
+            .then(() => dispatch(handleInitialData(answerObj.authUser)))
+            .then(() => dispatch(hideLoading()))
+    }
+}
+
+export function handleSaveQuestion(info) {
+    console.log('questionObj: ', info)
+    return dispatch => {
+        dispatch(showLoading())
+
+        return saveQuestion({
+            ...info,
+            author: info.authUser
+        })
+            .then(res => dispatch(handleInitialData(res.author)))
+            .then(() => dispatch(hideLoading()))
     }
 }
 
